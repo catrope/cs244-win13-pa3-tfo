d3b9b3c14995149d9f46dd60ed60cc30d73f98b0
Catrope <roan.kattouw@gmail.com>
2013-03-09 22:31:27 -0800

    Utility script to split Wireshark captures into HTTP requests and responses

---
+++ split.php
@@ -0,0 +1,12 @@
+<?php
+$in = file_get_contents( $argv[1] );
+$split = preg_split( '/((?:GET )|(?:HTTP\/1\.\d \d\d\d ))/', $in, -1, PREG_SPLIT_DELIM_CAPTURE );
+
+array_shift( $split );
+//var_dump($split);
+for ( $i = 0; $i < count( $split ); $i += 2 ) {
+	$n = (int)($i / 2);
+	file_put_contents( "{$argv[1]}.$n", $split[$i] . $split[$i+1] );
+}
+
+
