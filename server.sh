a3cb5a83ec67b6c5be741147b08633c814dacb27
Catrope <roan.kattouw@gmail.com>
2013-03-11 11:32:05 -0700

    grmbl

--- server.sh
+++ server.sh
@@ -4,5 +4,5 @@ PORT=12345
 
 echo Starting server for Wikipedia experiment on port $PORT
 cd wikipedia
-./tfomultiserver -p 12345 -f -r wikipedia/resp
+../tfomultiserver -p 12345 -f -r wikipedia/resp
 
