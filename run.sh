6de6043d41fc1e6300719ff0f1c4e3d79b2b95fb
Catrope <roan.kattouw@gmail.com>
2013-03-11 11:29:20 -0700

    Client and server scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,28 @@
+#! /bin/bash
+
+# CONFIGURE ME
+SERVER=ec2-54-241-232-199.us-west-1.compute.amazonaws.com
+SERVERIP=$(host $SERVER | cut -d ' ' -f 4)
+PORT=12345
+
 # Enable TFO
 echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen
 
-# TODO
+# Load Wikipedia 10 times without TFO
+cd wikipedia
+for i in {1..10}
+do
+	echo -n "Wikipedia without TFO run $i "
+	$(which time) -f '%e' ../tfomulticlient -s $SERVERIP -p $PORT -r reqs
+done
+
+# Load Wikipedia once with TFO so we have a cookie for sure
+../tfomulticlient -s $SERVERIP -p $PORT -r reqs -f
+
+# Load Wikipedia 10 times with TFO
+for i in {1..10}
+do
+	echo -n "Wikipedia with TFO run $i "
+	$(which time) -f '%e' ../tfomulticlient -s $SERVERIP -p $PORT -r reqs -f
+done
+
---
+++ server.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+PORT=12345
+
+echo Starting server on port $PORT
+./tfomultiserver -p 12345 -f
+
