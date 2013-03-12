7c7d91089b5c1dfd88f572579f8d16e0abada51a
Catrope <roan.kattouw@gmail.com>
2013-03-11 20:47:29 -0700

    Make split.php handle POSTs too

--- split.php
+++ split.php
@@ -1,6 +1,6 @@
 <?php
 $in = file_get_contents( $argv[1] );
-$split = preg_split( '/((?:GET )|(?:HTTP\/1\.\d \d\d\d ))/', $in, -1, PREG_SPLIT_DELIM_CAPTURE );
+$split = preg_split( '/((?:(?:GET|POST) )|(?:HTTP\/1\.\d \d\d\d ))/', $in, -1, PREG_SPLIT_DELIM_CAPTURE );
 
 array_shift( $split );
 //var_dump($split);
