55852d8af521495435f34e24a01abba8c74e8892
Catrope <roan.kattouw@gmail.com>
2013-03-11 18:28:06 -0700

    Take 20 samples and compute the average

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 SERVER=ec2-54-241-232-199.us-west-1.compute.amazonaws.com
 SERVERIP=$(host $SERVER | cut -d ' ' -f 4)
 PORT=12345
+SAMPLES=20
 
 # Enable TFO
 echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen
@@ -18,14 +19,14 @@ make
 # Load Wikipedia 10 times without TFO
 cd wikipedia
 SUM=0
-for i in {1..10}
+for i in {1..$SAMPLES}
 do
 	echo -n "Wikipedia without TFO run $i: "
 	T=$(../tfomulticlient -s $SERVERIP -p $PORT -r reqs)
 	echo $T us
 	SUM=$(($SUM + $T))
 done
-echo Total: $SUM us
+echo Average: $(($SUM / $SAMPLES)) us
 echo
 
 # Load Wikipedia once with TFO so we have a cookie for sure
@@ -33,12 +34,12 @@ echo
 
 # Load Wikipedia 10 times with TFO
 SUM=0
-for i in {1..10}
+for i in {1..$SAMPLES}
 do
 	echo -n "Wikipedia with TFO run $i: "
 	T=$(../tfomulticlient -s $SERVERIP -p $PORT -r reqs -f)
 	echo $T us
 	SUM=$(($SUM + $T))
 done
-echo Total: $SUM us
+echo Average: $(($SUM / $SAMPLES)) us
 
