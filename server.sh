dea6bbbf169517414e56f282c8926f2c5d658c57
Catrope <roan.kattouw@gmail.com>
2013-03-11 21:05:22 -0700

    Typo

--- server.sh
+++ server.sh
@@ -8,9 +8,8 @@ echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen
 echo Building...
 make
 
-for experiement in wikipedia amazon
+for experiment in wikipedia amazon
 do
-
 	echo Starting server for $experiment experiment on port $PORT
 	( cd $experiment && ../tfomultiserver -p $PORT -f -r resp ) &
 	PORT=$(($PORT + 1))
